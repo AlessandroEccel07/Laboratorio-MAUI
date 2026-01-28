83874085610121b848c49e669f2c4b6d61bb1fb3
AlessandroEccel07 <ixm775@student.edu.ti.ch>
2026-01-28 23:05:00 +0100

    Esercizio Equazione

--- LaboratorioMAUI/AppConvertitore/MainPage.xaml.cs
+++ LaboratorioMAUI/AppConvertitore/MainPage.xaml.cs
@@ -15,7 +15,7 @@
         {
             count++;
 
-            string Convertire = franchi.Text;
+            
 
 
 
--- LaboratorioMAUI/AppEquazione/MainPage.xaml.cs
+++ LaboratorioMAUI/AppEquazione/MainPage.xaml.cs
@@ -11,14 +11,36 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
+        
             count++;
 
             if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            {
+                if (double.TryParse(EntA.Text, out double a) && double.TryParse(EntB.Text, out double b) && double.TryParse(EntC.Text, out double c))
+                {
+                    double delta = b * b - 4 * a * c;
+                    if (delta > 0) 
+                    { 
+                        double x1= (-b + Math.Sqrt(delta)) / (2 * a);
+                        double x2= (-b - Math.Sqrt(delta)) / (2 * a);
+                        Risultato.Text = $"Due Soluzioni Reali Distinte: x1 = {x1}, x2 = {x2}";
+                    }
+                    else if (delta == 0) 
+                    { 
+                        double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                        Risultato.Text = $"Solo una soluzione x = {x1}";
+                    }
+                    else 
+                    { 
+                        Risultato.Text = $"Nessuna soluzione nell'insieme dei numeri reali";
+                    }
+                }
+                else
+                {
+                    Risultato.Text = $"Valori Invalido";
+                }
+                count = 0;
+            }
         }
     }
 
