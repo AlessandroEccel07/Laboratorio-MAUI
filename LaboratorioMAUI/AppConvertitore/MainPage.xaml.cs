e86566caf71181defc713b1bd5291c4ce899490f
AlessandroEccel07 <ixm775@student.edu.ti.ch>
2026-01-28 16:27:34 +0100

    Update per Esercizi di esempio
    + Inizio interfaccia ese 1

---
+++ LaboratorioMAUI/AppConvertitore/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace AppConvertitore
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        int count = 0;
+        double result = 0.0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            string Convertire = franchi.Text;
+
+
+
+            if (count == 1) {
+                if(double.TryParse(franchi.Text, out double fr))
+                {
+                    double eu = fr * 0.93;
+                    Risultato.Text = $"Risultato: {eu} €";
+                }
+                else
+                {
+                    Risultato.Text = $"Valore Invalido";
+                }
+                    count = 0;
+        }
+             
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
--- LaboratorioMAUI/AppEsempio/MainPage.xaml.cs
+++ LaboratorioMAUI/AppEsempio/MainPage.xaml.cs
@@ -20,6 +20,17 @@
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
+        private void OnMussiClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked Mussi {count} time";
+            else
+                CounterBtn.Text = $"Clicked Mussi {count} times";
+
+            SemanticScreenReader.Announce(MussiCounterButton.Text);
+        }
     }
 
 }
