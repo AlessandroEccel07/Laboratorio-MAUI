2357fdba39ac0683a201779b6df0622a21773d51
AlessandroEccel07 <ixm775@student.edu.ti.ch>
2026-03-18 15:43:10 +0100

    Aggiunto AppDiario
    Aggiunta classe Nota
    Formato pagina xaml

---
+++ LaboratorioMAUI/AppDiario/AppDiario/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace AppDiario
+{
+    public partial class MainPage : ContentPage
+    {
+        string percorsoFile = Path.Combine(FileSystem.AppDataDirectory, "note.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSalva_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnLeggi_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
