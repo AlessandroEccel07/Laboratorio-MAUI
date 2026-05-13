f938588f00815665de959b7bc76d272fbaa8e691
AlessandroEccel07 <ixm775@student.edu.ti.ch>
2026-05-13 15:41:56 +0200

    Esercizi Linq

---
+++ LaboratorioMAUI/EsercizioLinq/EsercizioLinq/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = Student.GetStudents();
+
+            var es1 = students.Where(n => n.Grade >= 70).ToList();
+            var es2 = students.Where(n => n.Branch=="Informatica").ToList();
+            var es3 = students.Any(n => n.Grade == 100);
+            var es4 = students.All(n => n.Grade > 40);
+            var es5 = students.FirstOrDefault(n=> n.Branch=="Telecomunicazioni").ToString();
+            var es6 = students.SingleOrDefault(n=>n.ID==1001).ToString();
+
+            var es7 = students.Select(n=>n.Name).ToList();
+            var es8 = students.Select(n => n.Name).ToList();
+            foreach(String x in es8)
+            {
+                Console.Write(x);
+            }
+
+        }
+    }
+}
---
+++ LaboratorioMAUI/HelloLinq/HelloLinq/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeri = new List<int>() { 1,7,3,4,5,6};
+            List<Studente> studenti = new List<Studente>();
+            studenti.Add(new Studente("Giuseppe", 20));
+            studenti.Add(new Studente("Simona", 104));
+
+            //Vogliamo ottenere i numeri pari dalla lista numeri
+            List<int> pari = numeri.Where(n  => n%2==0).ToList();
+
+            var nomi = studenti.Select(x=>x.Nome).ToList();
+            var crescenti = studenti.OrderBy(x => x).ToList();
+            foreach ( s in crescenti)
+            { 
+                Console.WriteLine(s);
+            }
+            
+        }
+    }
+}
